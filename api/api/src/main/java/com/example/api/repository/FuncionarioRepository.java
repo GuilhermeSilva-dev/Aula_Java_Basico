e0133e31f010a97ffbd4e65aca9325f716fc3eb1
GuilhermeSilva-dev <guilherme.s.souza7@ba.estudante.senai.br>
2026-04-01 21:14:16 -0300

    Add files via upload

---
+++ api/api/src/main/java/com/example/api/repository/FuncionarioRepository.java
@@ -0,0 +1,7 @@
+package com.example.api.repository;
+
+import com.example.api.model.FuncionarioModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FuncionarioRepository extends JpaRepository<FuncionarioModel, Long> {
+}
